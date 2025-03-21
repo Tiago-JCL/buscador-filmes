2da69c2ff6be3c07062976f32732da6787c806b9
volturifox <tiagoleite.work@gmail.com>
2025-03-21 01:29:52 -0300

    Mais Config

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"; // Importa o roteador
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 import "./index.css";
-import App from "./App.tsx";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <BrowserRouter basename="/buscador-filmes/"> {/* Define a base */}
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/buscador-filmes">
       <App />
     </BrowserRouter>
-  </StrictMode>
+  </React.StrictMode>
 );
